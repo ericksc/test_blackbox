33fc57be64e9a7708304c8dd7b29cfe2e6825737
Erick Salas-Chaverri <ericksc@gmail.com>
2019-03-08 15:06:29 -0600

    Initial commit

---
+++ test_blackbox.py
@@ -0,0 +1,11 @@
+from blackbox import blackbox
+
+print('Importando blackbox')
+
+def test_method1():
+
+    assert 1 == blackbox.method1(), 'debe ser  1'
+
+def test_method2():
+
+    assert 2 == blackbox.method2(), 'debe ser  2'
